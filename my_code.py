820853430541f1b862400032c08dc7600bac2a27
bjacobus <24bjacobus@athenian.org>
2020-10-28 12:18:14 -0700

    day one notes

--- my_code.py
+++ my_code.py
@@ -1,3 +1,18 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
-# Collaborators:               (fill this is on every assignment, even if the answer is "none")
+# Collaborators:               (fill this is on every assignment, even if the answer is "none"
 
+#x = 5
+#x = 7
+#print(x)
+#print(type(x))
+
+#num1 = 5.1
+#print(type(num1))
+
+#name = 'ben'
+#print(name)
+#print(f'hello {name}')
+
+print('who are you? ')
+name = input('who are you? ')
+print(f'hi {name}')
